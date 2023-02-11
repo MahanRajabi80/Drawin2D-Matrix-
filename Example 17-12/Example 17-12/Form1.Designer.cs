42fb9a5ef02864258904da914d0ef1d0210f42c5
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-02-11 19:53:42 +0330

    Add files via upload

---
+++ Example 17-12/Example 17-12/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Example_17_12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number Circle:";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(90, 145);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(57, 20);
+            this.txtNumber.TabIndex = 0;
+            this.txtNumber.Text = "150";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(172, 7);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(419, 385);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(43, 171);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(104, 29);
+            this.btnShow.TabIndex = 1;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 404);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Example 17-12 (Drawin2D , Matrix)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ Example 17-12/Example 17-12/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Example_17_12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //-----------------------------------------------------------------
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            int x = panel1.Width / 2;
+            int y = panel1.Height / 2;
+            int number = int.Parse(txtNumber.Text);
+            Pen p = new Pen(Color.Black);
+            Graphics gr = panel1.CreateGraphics();
+            gr.Clear(this.BackColor);
+            System.Drawing.Drawing2D.Matrix myMatrix = new System.Drawing.Drawing2D.Matrix();
+            for (int i = 1; i <= number; i++)
+            {
+                gr.DrawEllipse(p, panel1.Width-60, panel1.Height/2, 30, 30);
+                myMatrix.RotateAt(360.0F/number, new Point(x, y));
+                gr.Transform = myMatrix;
+            }
+            gr.Dispose();
+        }
+    }
+}
